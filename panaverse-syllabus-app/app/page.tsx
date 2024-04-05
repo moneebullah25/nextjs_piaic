bf268e06340a5c059f08af77db0c0363ed2a270f
Muneeb Ullah <moneebullah25@gmail.com>
2024-04-05 12:58:27 +0500

    added panaverse-syllabus app

---
+++ panaverse-syllabus-app/app/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import NavBar from './nav-bar';
+import Hero from "./hero"
+import Footer from './footer';
+
+export default function Home() {
+  return (
+    <>
+    
+    <Hero />
+    
+    </>
+  )
+}
